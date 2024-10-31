53e76d44efb4e7b302faf853207092816c889ead
dcho-jaewook <fermatjw@gmail.com>
2024-10-30 23:59:52 -0700

    Postpone Carousel

--- EduGate Website/Pages/DIS.js
+++ EduGate Website/Pages/DIS.js
@@ -1,20 +1,29 @@
-let currentSlide = 1;
+
+let currentSlide = 0;
+const books = document.querySelectorAll('.DIS-books-ele');
 
 function plusSlides(n) {
     console.log(n);
     currentSlide += n;
-    if (currentSlide > 4)
-        currentSlide = 1;
-    else if (currentSlide < 1)
-        currentSlide = 4;
+    if (currentSlide > 3)
+        currentSlide = 0;
+    else if (currentSlide < 0)
+        currentSlide = 3;
 
     adjustVisual();
 }
 
 function adjustVisual() {
-    let oldCurrent = document.getElementsByClassName("current");
-    oldCurrent[0].classList.remove("current");
-
-    let newCurrent = document.getElementsByClassName("DIS-books-ele")[currentSlide-1];
-    newCurrent.classList.add("current");
+    console.log("Call");
+    console.log(books);
+    books.forEach((book, index) => {
+        console.log(book);
+        book.classList.remove('current', 'next', 'prev');
+        if (index === currentSlide)
+            book.classList.add("current");
+        else if (index === (currentSlide - 1 + books.length) % books.length)
+            book.classList.add("prev");
+        else if (index === (currentSlide + 1) % books.length)
+            book.classList.add("next");
+    });
 }
